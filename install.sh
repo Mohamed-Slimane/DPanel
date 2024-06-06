c16cbeef54b015c02ff5f7d6389201066d1d7504
Mohamed Slimane <mohamedsoliman852@gmail.com>
2024-06-06 23:13:31 +0100

    2.4

--- install.sh
+++ install.sh
@@ -1,57 +1,64 @@
 #!/bin/bash
+
+# Update packages
 apt update
-apt-get install -y unzip
-apt-get install -y python3
-apt-get install -y python3-venv
-apt-get install -y python3-pip
-ufw allow 8080
+
+# Install required packages
+apt-get install -y unzip python3 python3-venv python3-pip ufw wget
+
+# Allow port 8752 through firewall
+ufw allow 8752
+
+# Define Dpanel variables
 dpanel_url="https://dpanel.de-ver.com/api/download/dpanel.zip"
 dpanel_dir="/var/server/dpanel/app/"
 dpanel_venv="/var/server/dpanel/venv/"
+dpanel_service="/etc/systemd/system/dpanel.service"
+
+# Create directories
 mkdir -p "$dpanel_dir"
-wget -N "$dpanel_url" -P "$dpanel_dir" && unzip -oqq "$dpanel_dir/dpanel.zip" -d "$dpanel_dir"
-rm $dpanel_dir/dpanel.zip
+
+# Download and extract Dpanel
+wget -N "$dpanel_url" -P "$dpanel_dir" && unzip -oqq "$dpanel_dir/dpanel.zip" -d "$dpanel_dir" && rm "$dpanel_dir/dpanel.zip"
+
 if [ $? -eq 0 ]; then
   echo "ZIP file downloaded and extracted successfully to '$dpanel_dir'."
-  python3 -m venv /var/server/dpanel/venv
-  /var/server/dpanel/venv/bin/pip install django
-  /var/server/dpanel/venv/bin/pip install django_widget_tweaks
-  # make migaration
-  /var/server/dpanel/venv/bin/python /var/server/dpanel/app/manage.py makemigrations
-  /var/server/dpanel/venv/bin/python /var/server/dpanel/app/manage.py migrate
+
+  # Setup Python virtual environment
+  python3 -m venv "$dpanel_venv"
+  "$dpanel_venv/bin/pip" install django django_widget_tweaks
+
+  # Run database migrations
+  "$dpanel_venv/bin/python" "$dpanel_dir/manage.py" makemigrations
+  "$dpanel_venv/bin/python" "$dpanel_dir/manage.py" migrate
+
   # Create superuser
-  dpanel_password = here rand passworrd
   dpanel_password=$(python3 -c "import random; print(''.join([str(random.randint(0, 9)) for _ in range(10)]))")
-  /var/server/dpanel/venv/bin/python /var/server/dpanel/app/manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@localhost', '$dpanel_password')"
-  # create service
-  dpanel_service="/etc/systemd/system/dpanel.service"
-  if [ -f "$dpanel_service" ]; then
-      echo "Service file already exists at $dpanel_service."
-  fi
-  service_content="[Unit]
+  "$dpanel_venv/bin/python" "$dpanel_dir/manage.py" shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@localhost', '$dpanel_password')"
+
+  # Create systemd service
+  echo "[Unit]
 Description=DPanel service
 After=network.target
+
 [Service]
 Group=www-data
-WorkingDirectory=/var/server/dpanel/app
-ExecStart=/var/server/dpanel/venv/bin/python /var/server/dpanel/app/manage.py runserver 0.0.0.0:8080
+WorkingDirectory=$dpanel_dir
+ExecStart=$dpanel_venv/bin/python $dpanel_dir/manage.py runserver 0.0.0.0:8752
 Restart=always
+
 [Install]
-WantedBy=multi-user.target
-  "
-  echo "$service_content" | sudo tee "$dpanel_service"
-  if [ -f "$dpanel_service" ]; then
-      echo "Service file created successfully at $dpanel_service."
-      systemctl enable dpanel
-      systemctl start dpanel
-  else
-      echo "Failed to create the service file."
-  fi
-  local_ip=$(hostname -I | tr -d ' ')
+WantedBy=multi-user.target" | sudo tee "$dpanel_service" >/dev/null
+  echo "Service file created successfully at $dpanel_service."
+  systemctl enable dpanel
+  systemctl start dpanel
+
+  # Get publick server ip
+  local_ip=$(hostname -I | awk '{print $1}')
   echo "
   ----------------------------------------------------------------------------------------------
   Dpanel has been installed successfully.
-  Login to your Dpanel at http://$local_ip:8080
+  Login to your Dpanel at http://$local_ip:8752
   Username: admin
   Password: $dpanel_password
   Please change the password after first login to prevent unauthorized access to your panel.
